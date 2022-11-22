7bf14ddee87d1cf1a0c46fcd36f8edec78a8a080
Mohira21 <muminovamohiraa@gmail.com>
2022-11-22 19:08:36 +0500

    task6 added

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -1,1 +1,28 @@
 ﻿
+
+Console.WriteLine("Enter your number : ");
+
+int n = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Whould you like to compute the sum or compute th product? \n " +
+    "for compute the sum choose 1, " +
+    "for compute th product choose 2");
+
+int choose = int.Parse(Console.ReadLine());
+
+if (choose == 1)
+{
+    int sum = 0;
+    for (int i = 1; i <= n; i++)
+        sum += i;
+    Console.WriteLine(sum);
+}
+if (choose == 2)
+{
+    int sum = 0;
+    for (int i = 1; i <= n; i++)
+        sum *= i;
+    Console.WriteLine(sum);
+}
+else Console.WriteLine("incorrect choice");
